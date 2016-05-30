675bf3d9f3df7787780c324bd51532b723d2b77e
elzup <guild0105@gmail.com>
2016-05-30 15:09:51 +0900

    Refactor: format code

--- SharePlayMusic/js/background.js
+++ SharePlayMusic/js/background.js
@@ -8,34 +8,34 @@ var hashtags = "ぷれみゅ";
 
 //右クリックした時にcontentScriptから飛んできたタイトル情報を受け取ってtitleに格納
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if(request.type == "title"){
-            tabtitle = request.value;
-        }
-        if(request.type == "albumart"){
-            arturl = request.value;
-        }
-
+  function (request, sender, sendResponse) {
+    if (request.type == "title") {
+      tabtitle = request.value;
     }
+    if (request.type == "albumart") {
+      arturl = request.value;
+    }
+
+  }
 );
 
 //コンテクストメニューがクリックされた時に実行される
-function tweet(){
-    //alert("in background, tweet()");
-    text = tabtitle;
-    window.console.log("in background:" + tabtitle);
-    //console.log("in background:"+ arturl);
-    window.open(baseUrl + "?&text=" + encodeURI(tabtitle) + "&hashtags=" + hashtags);
-}
+var tweet = function () {
+  //alert("in background, tweet()");
+  text = tabtitle;
+  window.console.log("in background:" + tabtitle);
+  //console.log("in background:"+ arturl);
+  window.open(baseUrl + "?&text=" + encodeURI(tabtitle) + "&hashtags=" + hashtags);
+};
 
-chrome.contextMenus.create({title: "tweet", onclick: tweet}, function(){
-    //alert("コンテキストメニュー登録完了");
+chrome.contextMenus.create({title: "tweet", onclick: tweet}, function () {
+  //alert("コンテキストメニュー登録完了");
 });
 
-var getTags = function(){
-    return hashtags;
-}
+var getTags = function () {
+  return hashtags;
+};
 
 var updateTags = function (tags) {
-    hashtags = tags;
-}
+  hashtags = tags;
+};
--- SharePlayMusic/js/popup.js
+++ SharePlayMusic/js/popup.js
@@ -1,10 +1,10 @@
-$(function(){
-    var BG = chrome.extension.getBackgroundPage();
-    $('#tags').val(BG.getTags());
-    $('#update').click(function(){
-        BG.updateTags( $('#tags').val() );
-    });
-    $('#tweet').click(function(){
-        BG.tweet();
-    });
+$(function () {
+  var BG = chrome.extension.getBackgroundPage();
+  $('#tags').val(BG.getTags());
+  $('#update').click(function () {
+    BG.updateTags($('#tags').val());
+  });
+  $('#tweet').click(function () {
+    BG.tweet();
+  });
 });
